7dde11ee4da66cb05c0b7a3e11498c160df58010
Muhammed Acar <muhammed.acar@sabah.com.tr>
2025-05-28 16:41:05 +0300

    - Ready for jitpack test release

--- app/src/main/java/org/turkuvaz/MainActivity.kt
+++ app/src/main/java/org/turkuvaz/MainActivity.kt
@@ -22,7 +22,8 @@ class MainActivity : ComponentActivity() {
                 /*
                 MavenLocal
                     1- turkuvapp build.gradle içinde plugins kısmına id("maven-publish") ekliyoruz
-                    2- yine aynı dosyada en alta aşağıdaki kodları ekliyoruz
+                    2- aynı dosya içerisinde isMinifyEnabled false olmalı
+                    3- yine aynı dosyada en alta aşağıdaki kodları ekliyoruz
                         afterEvaluate {
                             android.libraryVariants.forEach { variant ->
                                 publishing.publications.create(variant.name, MavenPublication::class.java){
@@ -33,17 +34,23 @@ class MainActivity : ComponentActivity() {
                                 }
                             }
                         }
-                    3- modülün localde yayınlanması için gradle tarafında publishToMavenLocal komutunu çalıştırıyoruz
-                    4- Kütüphaneyi localde test edeceğimiz projenin settings.gradle dosyasında dependencyResolutionManagement altında repositories kısmına mavenLocal() kodunu ekliyoruz
-                    5- Test edeceğimiz projenin app seviyesindeki build.gradle dosyasına implementation("org.turkuvaz:turkuvapp:1.0.1") ekleyip kütüphaneyi çağırabiliyoruz
+                    4- modülün localde yayınlanması için gradle tarafında publishToMavenLocal komutunu çalıştırıyoruz (userPath/.m2/repository klasörüne .jar ve .aar dosyalarını export eder)
+                    5- Kütüphaneyi localde test edeceğimiz projenin settings.gradle dosyasında dependencyResolutionManagement altında repositories kısmına mavenLocal() kodunu ekliyoruz
+                    6- Test edeceğimiz projenin app seviyesindeki build.gradle dosyasına implementation("org.turkuvaz:turkuvapp:1.0.1") ekleyip kütüphaneyi çağırabiliyoruz
 
-                Jitpack (yukarıdaki işlemlerden sadece 2. madde gerekli)
+                Jitpack (yukarıdaki işlemlerden sadece 2. ve 3. madde gerekli)
                     1- Kütüphane modülünün oluşturulduğu projede jitpack.yml dosyasını oluşturuyoruz. Dosya içeriği aşağıdaki gibi olmalı
                         jdk:
                             - openjdk11
-                    2- Proje github'a push edilmeli ve repo ismi ile artifactId aynı olmalı.
-                    3-
+                    2- Proje artifactId ile aynı isme sahip olan repo'ya push edilmeli
+                    3- Github sayfasında sağdaki Releases'a tıklayarak yeni tag ile birlikte release oluşturuyoruz
+                    4- jitpack.io websitesine github hesabıyla login oluyoruz (diğer sekmede github açık ise direkt plugin(access) kurulum izni istiyor)
+                    5- Sitenin sol kısmındaki Repositories altında modülü seçip get it butonuna basarak birkaç dakika analiz etmesini bekliyoruz. Analiz sonucunda log sütununa yeşil icon gelirse sorun yok/hazır anlamındadır
+                    6- Test edeceğimiz projenin settings.gradle dosyasında dependencyResolutionManagement altında repositories kısmına 'maven { url = uri("https://jitpack.io") }' kodunu ekliyoruz
+                    7- Yine test projesinde app seviyesindeki build.gradle dosyasına analiz sonucu jitpack sitesinin verdiği implementation'ı ekliyoruz ve artık kütüphaneyi çağırabiliyoruz
                 */
+
+
             }
         }
     }
