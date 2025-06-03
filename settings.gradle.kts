13e505f6a159dfe574b530b8a06b695e7f003050
Muhammed Acar <muhammed.acar@sabah.com.tr>
2025-06-03 19:49:00 +0300

    - MavenCentral custom namespace test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,3 +22,4 @@ dependencyResolutionManagement {
 rootProject.name = "TMDLibrary"
 include(":app")
 include(":turkuvapp")
+include(":testcom")
---
+++ testcom/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.29.0"
+}
+
+android {
+    namespace = "com.mehmetkirazli.testcom"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    signingConfigs {
+        create("release") {
+            storeFile = file("../turkuvapp/key_file")
+            storePassword = "test12"
+            keyAlias = "test"
+            keyPassword = "test12"
+        }
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("release")
+        }
+        debug {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    android.libraryVariants.forEach { variant ->
+        publishing.publications.create(variant.name, MavenPublication::class.java) {
+            from(components.findByName(variant.name))
+            groupId = "com.mehmetkirazli"
+            artifactId = "turkuvapp"
+            version = "3.0.0"
+        }
+    }
+}
